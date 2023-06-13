1f000f582359c966d790619a84808a66c1d4cb5c
Paul Morgan <orbitingmind@gmail.com>
2023-06-12 23:50:30 -0400

    new componets and functions

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import React ,{useState} from 'react';
 import './App.css';
 
 function App() {   
-  const [airQualityData, setAiraualityData] = useState(null)
-
+  const [airQualityData, setAirQualityData] = useState(null)
+  const [error, setError] = useState(null)
 
   const getAirquality = async(city) =>{   
       try{
           const response = await fetch(`https://api.waqi.info/feed/${city}}/?token=${process.env.REACT_APP_API_TOKEN}`)
           const data = await response.json() 
           console.log(data)
-      
+        if(response.ok && data.status ==="ok" ){
+          setAirQualityData(data.data) 
+          setError(null)
+        } 
+        else {
+          setError("sorry we  cannot find the city you are looking for")
+          setAirQualityData(null)
+        }
         } 
       catch(error){ 
         console.log("network error",error)
-          
+         setError("something went wrong")
+         setAirQualityData(null) 
       }
 
 
@@ -27,9 +35,13 @@ function App() {
 
 
   return (
+    <div>
     <h1>
-      Hello
-    </h1>
+      Air Quality
+    </h1>  
+
+    <CitySearch getAirquality={getAirquality} />
+    </div>
   );
 }
 
---
+++ src/CitySearch.js
@@ -0,0 +1,49 @@
+import React, {useState} from "react"; 
+
+export default function CitySearch({getAirQuality}) {
+    
+    const [inputValue, setInputValue] = useState("")
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value)
+    }
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+
+
+
+    }
+
+    return(
+            <form onSubmit={handleSearch}>
+
+              <input type="text" placeholder="enter city ...." onChange={handleInputChange}>
+
+              </input> 
+
+              <button type="submit">
+                Search
+              </button> 
+            </form>
+
+
+
+
+
+
+
+
+
+
+
+    )
+
+
+
+
+
+
+
+
+}
